19d30463b8953b003325a88fe4111d79c8c63501
Christian Scott <christianfscott@gmail.com>
2016-04-21 13:10:20 +1200

    read api key from json
    unit_conversion now tries to find the currencylayer API key in a file
    called “api_keys.json” and prompts the user for a key if it can’t find
    one

--- numbers/unit_conversion.py
+++ numbers/unit_conversion.py
@@ -8,70 +8,74 @@ import json, datetime, re, sys
 from urllib2 import urlopen
 
 def is_numerical(str):
-	try:
-		float(str)
-		return True
-	except ValueError:
-		return False
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
 
 def get_input():
-	amount = str(raw_input("Enter amount to convert: ")).replace(' ', '')
+    amount = str(raw_input("Enter amount to convert: ")).replace(' ', '')
 
-	while not is_numerical(amount):
-		raw_input("Non-numerical amount entered. Press enter to try again.")
-		amount = str(raw_input("Enter amount to convert: ")).replace(' ', '')
+    while not is_numerical(amount):
+        raw_input("Non-numerical amount entered. Press enter to try again.")
+        amount = str(raw_input("Enter amount to convert: ")).replace(' ', '')
 
-	from_currency = str(raw_input("Enter your source currency (3 digit code): ")).upper().replace(' ', '')
-	to_currency = str(raw_input("Enter the currency you would like to convert to (3 digit code): ")).upper().replace(' ', '')
+    from_currency = str(raw_input("Enter your source currency (3 digit code): ")).upper().replace(' ', '')
+    to_currency = str(raw_input("Enter the currency you would like to convert to (3 digit code): ")).upper().replace(' ', '')
 
-	return amount, from_currency, to_currency
+    return amount, from_currency, to_currency
 
 def load_response(api_key, from_currency, to_currency):
-	request = urlopen('http://apilayer.net/api/live?access_key=%s&currencies=%s,%s&format=1' 
-		% (api_key, from_currency, to_currency))
-	response = json.loads(request.read())
+    request = urlopen('http://apilayer.net/api/live?access_key=%s&currencies=%s,%s&format=1' 
+        % (api_key, from_currency, to_currency))
+    response = json.loads(request.read())
 
-	try:
-		from_rate = response['quotes']['USD%s' % (from_currency)]
-		to_rate = response['quotes']['USD%s' % (to_currency)]
-		return from_rate, to_rate
-	except Exception, e:
-		return e
+    try:
+        from_rate = response['quotes']['USD%s' % (from_currency)]
+        to_rate = response['quotes']['USD%s' % (to_currency)]
+        return from_rate, to_rate
+    except Exception, e:
+        return e
 
 def currency():
-	if len(sys.argv) == 1:
-		amount, from_currency, to_currency = get_input()
+    try:
+        api_key = json.loads(open('api_keys.json').read())['currency']
+    except:
+        api_key = str(raw_input("Please enter your currencylayer API key: "))
 
-	elif len(sys.argv) == 4:
-		args = [arg.upper().replace(' ', '') for arg in sys.argv][1:]
-		amount, from_currency, to_currency = args
-		while not is_numerical(amount):
-			raw_input("Non-numerical amount entered. Press enter to try again.")
-			amount = str(raw_input("Enter amount to convert: ")).replace(' ', '')
+    if len(sys.argv) == 1:
+        amount, from_currency, to_currency = get_input()
 
-	else:
-		print "Wrong number of command line arguments."
-		amount, from_currency, to_currency = get_input()
+    elif len(sys.argv) == 4:
+        args = [arg.upper().replace(' ', '') for arg in sys.argv][1:]
+        amount, from_currency, to_currency = args
+        while not is_numerical(amount):
+            raw_input("Non-numerical amount entered. Press enter to try again.")
+            amount = str(raw_input("Enter amount to convert: ")).replace(' ', '')
 
-	response = load_response(api_key, from_currency, to_currency)
+    else:
+        print "Wrong number of command line arguments."
+        amount, from_currency, to_currency = get_input()
 
-	while type(response) != tuple:
-		raw_input('\nYou have entered one or more invalid currency codes. Press enter to try again.')
-		amount, from_currency, to_currency = get_input()
-		response = load_response(api_key, from_currency, to_currency)
+    response = load_response(api_key, from_currency, to_currency)
 
-	from_rate, to_rate = response
+    while type(response) != tuple:
+        raw_input('\nYou have entered one or more invalid currency codes. Press enter to try again.')
+        amount, from_currency, to_currency = get_input()
+        response = load_response(api_key, from_currency, to_currency)
 
-	to_amount = (float(amount) / float(from_rate)) * float(to_rate)
+    from_rate, to_rate = response
 
-	time = re.findall(r"\d+-\d+-\d+", str(datetime.datetime.now()))[0]
+    to_amount = (float(amount) / float(from_rate)) * float(to_rate)
 
-	print "\nAs of %s, %s %s is worth %.2f %s\n" % (time, amount, 
-						from_currency, round(to_amount, 2), to_currency)
+    time = re.findall(r"\d+-\d+-\d+", str(datetime.datetime.now()))[0]
+
+    print "\nAs of %s, %s %s is worth %.2f %s\n" % (time, amount, 
+                        from_currency, round(to_amount, 2), to_currency)
 
 def main():
-	currency()
-	pass
+    currency()
 
 if __name__ == "__main__":
-	main()
+    main()
