b4fd3f7795e339ea8787b7cf4e2bb3d68bf58fc0
Christian Scott <christianfscott@gmail.com>
2016-04-24 20:48:19 +1200

    add credit card validator

---
+++ numbers/credit_card_validator.py
@@ -0,0 +1,31 @@
+"""
+credit card validator
+
+credit to Mike Hall for details on the Luhn algorithm
+(http://www.brainjar.com/js/validation/default2.asp).
+
+author: christian scott
+"""
+
+sample = "1234567890123452"
+
+def validator(card_number):
+    double = []
+    for i, digit in enumerate(card_number[::-1]):
+        if i % 2 == 1:
+            double += [int(digit) * 2]
+        else:
+            double += [int(digit)]
+
+    total = 0
+    for digit in ''.join(map(str, double)):
+        total += int(digit)
+
+    if total % 10 == 0 and total != 0:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    print("{0} {1} a valid credit card number.".format(sample,
+                            "is" if validator(sample) else "is not"))
