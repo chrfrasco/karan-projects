5d7e8225a27742db4f9416b31f6efd862ae04f16
Christian <christianfscott@gmail.com>
2016-06-07 23:17:56 +1200

    add sieve

---
+++ classic_algorithms/sieve_of_eratosthenes.py
@@ -0,0 +1,14 @@
+from math import sqrt, ceil
+
+def sieve(upto):
+    A = [0, 0] + [True]*(upto-1)
+    primes = []
+    for i in range(2, ceil(sqrt(upto))):
+        if A[i]:
+            for j in range(i**2, upto, i):
+                A[j] == False
+    print(A)
+    return [num*boo for num, boo in zip(range(upto), A)]
+
+print(sieve(30))
+    
